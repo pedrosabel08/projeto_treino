dee1dfea76f23ef27b0ed1089eab4dbe00a03160
pedrosabel08 <pedrosabel08@gmail.com>
2024-02-29 19:17:30 -0300

    Create Treino.java

---
+++ Projeto Treino/src/modelo/Treino.java
@@ -0,0 +1,35 @@
+package modelo;
+
+public class Treino {
+	
+	int id, idUsuario;
+	String data, nomeTreino;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getNomeTreino() {
+		return nomeTreino;
+	}
+	public void setNomeTreino(String nomeTreino) {
+		this.nomeTreino = nomeTreino;
+	}
+	
+	
+
+}
