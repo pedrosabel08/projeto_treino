3997daf3eb2bbcbfb6376f49f4b07bb872724cca
pedrosabel08 <pedrosabel08@gmail.com>
2024-02-29 19:19:11 -0300

    Create Usuario.java

---
+++ Projeto Treino/src/modelo/Usuario.java
@@ -0,0 +1,35 @@
+package modelo;
+
+public class Usuario {
+	
+	int id;
+	String nomeUsuario, senha, tipo;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
