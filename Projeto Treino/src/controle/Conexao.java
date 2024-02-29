2891dde4086eccd0836be25b223c54528d2f4449
pedrosabel08 <pedrosabel08@gmail.com>
2024-02-29 19:14:59 -0300

    Update Conexao.java

--- Projeto Treino/src/controle/Conexao.java
+++ Projeto Treino/src/controle/Conexao.java
@@ -1,5 +1,27 @@
 package controle;
 
-public class Conexao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Conexao {
+	static Connection conexao;
+
+	public static Connection faz_conexao() {
+		try {
+			conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/bdtreino", "root", "");
+
+		} catch (SQLException e) {
+			System.out.println("Erro ao conectar a base de dados.");
+		}
+		return conexao;
+	}
+
+	public void desconectar() {
+		try {
+			conexao.close();
+		} catch (Exception e) {
+
+		}
+	}
 }
