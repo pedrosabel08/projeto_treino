333863e4607859598126dc6ee386ad44638c6916
pedrosabel08 <pedrosabel08@gmail.com>
2024-02-29 19:16:54 -0300

    Create Exercicio.java

---
+++ Projeto Treino/src/modelo/Exercicio.java
@@ -0,0 +1,47 @@
+package modelo;
+
+public class Exercicio {
+	
+	int id, series, reps, idTreino;
+	String nomeExercicio, tipo;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getSeries() {
+		return series;
+	}
+	public void setSeries(int series) {
+		this.series = series;
+	}
+	public int getReps() {
+		return reps;
+	}
+	public void setReps(int reps) {
+		this.reps = reps;
+	}
+	public int getIdTreino() {
+		return idTreino;
+	}
+	public void setIdTreino(int idTreino) {
+		this.idTreino = idTreino;
+	}
+	public String getNomeExercicio() {
+		return nomeExercicio;
+	}
+	public void setNomeExercicio(String nomeExercicio) {
+		this.nomeExercicio = nomeExercicio;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
